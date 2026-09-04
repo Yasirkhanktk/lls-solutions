140ceb7a1c2d69dd3e9e6cf7976c95acb940e50c
Yasirkhanktk <119963833+Yasirkhanktk@users.noreply.github.com>
2026-09-04 20:09:09 +0500

    fix(vite): set VITE_CONFIG_NATIVE_IGNORE_WARNING=true

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+process.env.VITE_CONFIG_NATIVE_IGNORE_WARNING = 'true'
+
 import { defineConfig, type HtmlTagDescriptor, type Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
